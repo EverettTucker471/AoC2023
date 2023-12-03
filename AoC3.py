2f42420cad91b41f4cc8d2dabff7728a12079041
EverettTucker471 <tucker24e@ncssm.edu>
2023-12-03 14:37:45 -0500

    Day 3 - A Little shorter

--- AoC3.py
+++ AoC3.py
@@ -74,32 +74,22 @@ def solve2():
             if list[i][j] == '*':
                 gears.append([i, j])
 
-    print(gears)
-
     for gear in gears:
         adjacent = set()
 
         row = gear[0]
         col = gear[1]
 
-        if getNumber(list, row - 1, col - 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row - 1, col - 1, rows, cols))
-        if getNumber(list, row - 1, col, rows, cols) is not None:
-            adjacent.add(getNumber(list, row - 1, col, rows, cols))
-        if getNumber(list, row - 1, col + 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row - 1, col + 1, rows, cols))
-        if getNumber(list, row, col - 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row, col - 1, rows, cols))
-        if getNumber(list, row, col + 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row, col + 1, rows, cols))
-        if getNumber(list, row + 1, col - 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row + 1, col - 1, rows, cols))
-        if getNumber(list, row + 1, col, rows, cols) is not None:
-            adjacent.add(getNumber(list, row + 1, col, rows, cols))
-        if getNumber(list, row + 1, col + 1, rows, cols) is not None:
-            adjacent.add(getNumber(list, row + 1, col + 1, rows, cols))
+        adjacent.add(getNumber(list, row - 1, col - 1, rows, cols))
+        adjacent.add(getNumber(list, row - 1, col, rows, cols))
+        adjacent.add(getNumber(list, row - 1, col + 1, rows, cols))
+        adjacent.add(getNumber(list, row, col - 1, rows, cols))
+        adjacent.add(getNumber(list, row, col + 1, rows, cols))
+        adjacent.add(getNumber(list, row + 1, col - 1, rows, cols))
+        adjacent.add(getNumber(list, row + 1, col, rows, cols))
+        adjacent.add(getNumber(list, row + 1, col + 1, rows, cols))
 
-        print(adjacent)
+        adjacent = set(filter(lambda item: item is not None, adjacent))
 
         ratio = 1
         if len(adjacent) == 2:
@@ -116,7 +106,7 @@ def getNumber(arr, row, col, rows, cols):
 
     num = []
     pos = col + 1
-    while col >= 0:
+    while col > 0:
         if arr[row][col].isdigit():
             num.insert(0, int(arr[row][col]))
         else:
@@ -130,11 +120,11 @@ def getNumber(arr, row, col, rows, cols):
             break
         pos += 1
 
-    power = 0
+    mult = 1
     rtn = 0
     for digit in reversed(num):
-        rtn += digit * (10 ** power)
-        power += 1
+        rtn += digit * mult
+        mult *= 10
 
     return rtn
 
